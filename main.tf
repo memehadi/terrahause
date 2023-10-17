67f1922e99144624c3d970dbc83be4bcbef39b7d
Musab Mehadi <mussawayus3@gmail.com>
2023-10-17 23:14:08 +0000

    added ability to pass variables to terrafirm cloud( terraform.auto.tfvars )

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
@@ -13,8 +11,8 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
