70ea14365ea3e36d7c95debd66afa15e98237a34
Musab Mehadi <mussawayus3@gmail.com>
2023-10-31 12:41:24 +0000

    added cdn for static website hosting

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -1,3 +1,5 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
+data "aws_caller_identity" "current" {}
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "website_bucket" {
@@ -38,4 +40,100 @@ resource "aws_s3_object" "error_html" {
   source = var.error_html_filepath
 
   etag = filemd5(var.error_html_filepath)
+}
+
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+  policy = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = {
+      "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
+      "Effect" = "Allow",
+      "Principal" = {
+        "Service" = "cloudfront.amazonaws.com"
+      },
+      "Action" = "s3:GetObject",
+      "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+      "Condition" = {
+      "StringEquals" = {
+          #"AWS:SourceArn": data.aws_caller_identity.current.arn
+          "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+        }
+      }
+    }
+  })
+}
+
+
+
+
+
+
+### cdn ##########
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_origin_access_control
+# https://aws.amazon.com/blogs/networking-and-content-delivery/amazon-cloudfront-introduces-origin-access-control-oac/
+resource "aws_cloudfront_origin_access_control" "default" {
+  name   = "OAC ${var.bucket_name}"
+  description  = "Origin Access Controls for Static Website Hosting ${var.bucket_name}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior  = "always"
+  signing_protocol  = "sigv4"
+}
+
+locals {
+  s3_origin_id = "MyS3Origin"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Static website hosting for: ${var.bucket_name}"
+  default_root_object = "index.html"
+
+  #aliases = ["mysite.example.com", "yoursite.example.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
